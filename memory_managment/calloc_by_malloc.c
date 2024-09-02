17506343119128978c6c001ec5a7be011907344d
KartikThakur2005 <kartikthakursaklani@gmail.com>
2024-09-02 14:58:28 +0000

    malloc

---
+++ memory_managment/calloc_by_malloc.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+//     // number of bytes
+//     int n;  
+
+//     char* arr = (int*) malloc(n);
+
+//     printf(" enter n");
+//     scanf("%d",&n);
+    
+   
+//    for (int i = 0; i < n; i++)
+//    {
+//       *(arr + i) = '\0';
+
+//    }
+   
+
+//     return 0;
+
+ 
+
+ 
+   return 0;
+}
